225e531d4db6a75337d49dbc52e4b23acc4dc3d3
nanderson87 <nicolas.anderson@mercadolibre.com>
2021-05-05 13:20:58 -0300

    Agrega ejemplo concurrencia_vs_paralelismo

---
+++ src/main/java/concurrencia_vs_paralelismo/EjemploIII.java
@@ -0,0 +1,9 @@
+package concurrencia_vs_paralelismo;
+
+public class EjemploIII {
+  public static void main(String[] args) {
+    System.out.println("Soy un programa que imprime ping-pong");
+    System.out.println("ping");
+    System.out.println("pong");
+  }
+}
